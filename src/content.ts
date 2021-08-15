2d1b5c1e7d8d192c384152aa1d4830acbcbdb4bd
jacremer <jacremer@live.com>
2021-08-15 16:47:24 -0700

    move eback to regex filtering on base input tags

--- src/content.ts
+++ src/content.ts
@@ -1,17 +1,36 @@
+import { format } from "path/posix";
 import { Message, Elements } from "./mod";
 
 type SendResponse = (response?: Elements) => void;
 
+const genCssSelector = (
+  tag?: string,
+  pattern?: string,
+  attrs?: Array<string>
+): string =>
+  tag
+    ? attrs
+      ? attrs
+          .reduce((acc, attr) => acc + `${tag}[${attr}*="${pattern}" i],`, "")
+          .slice(0, -1)
+      : `${tag}`
+    : pattern ?? "";
+
 // Finds matches on the current page and sends them to the caller
-const getFocusedElems = (selector: string, sendResponse: SendResponse) =>
-	sendResponse(Array.from(document.querySelectorAll(selector)));
+const getFocusedElems = (selector: string) =>
+  Array.from(document.querySelectorAll(selector));
 
 // Listens for parse calls and parses the page in focus
 chrome.runtime.onMessage.addListener(
-	(request: Message, _, sendResponse: SendResponse) => {
-		if (request === Message.Parse) {
-			const selector = 'coupon, .coupon, #coupon, discount, .discount, #discount';
-			getFocusedElems(selector, sendResponse);
-		}
-	}
+  (request: Message, _, sendResponse: SendResponse) => {
+    if (request === Message.Parse) {
+      const elems = getFocusedElems("input");
+      console.log(elems);
+      sendResponse(
+        elems.filter((e: Element) =>
+          e.innerHTML.match(/(coupon|discount|code)/i)
+        )
+      );
+    }
+  }
 );
